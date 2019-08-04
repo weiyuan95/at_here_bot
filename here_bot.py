99ec420091ef59ca723b494e80161290ebd86054
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2019-08-04 18:30:50 +0800

    get config settings from config file

--- here_bot.py
+++ here_bot.py
@@ -1,4 +1,14 @@
+from config import Configs
+import requests as r
+
 class HereBot:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.base_url = f'https://api.telegram.org/bot{self.api_key}/'
+    def __init__(self):
+        configs = Configs()
+        self.api_key = configs.api_key
+        self.base_url = f'https://api.telegram.org/bot{self.api_key}'
+
+    def auth_check(self):
+        get_bot_url = f'{self.base_url}/getMe'
+        resp = r.post(get_bot_url)
+
+        return resp.json()

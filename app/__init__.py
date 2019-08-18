70f5b3d8600526fa423a57b8b8a6610e6af9f2d5
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2019-08-18 17:43:11 +0800

    add logging for webhook branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from dotenv import load_dotenv
 from os.path import join, dirname
+import logging
 
 app = Flask(__name__)
+
 env_path = join(dirname(__file__), '..', '.env')
 load_dotenv(env_path)
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
+import os
 import app.bot.HereBot as HereBot
+from flask import request
+import logging
 
 @app.route('/')
 def index():
@@ -8,3 +11,8 @@ def index():
 @app.route('/key_check')
 def bot_test():
     return HereBot.auth_check()
+
+@app.route(f'/{os.getenv("HOOK_SECRET")}')
+def get_updates():
+    logging.info(f'{request.json}\n\n')
+    return f'{request.json}'
